54e2ed4291cb2685f561332786b4b95b9cf5bb20
triple <45209813+Triple1996@users.noreply.github.com>
2023-05-06 21:32:20 -0400

    Begin setting up CSS grid

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
 const gridContainer = document.querySelector('#grid-container');
 
 // create a 16 x 16 grid of divs
-boxItem = document.createElement('div');
-boxItem.setAttribute('class', 'created');
-
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(boxItem);
-gridContainer.appendChild(document.createElement('button'));
+for (let i = 0; i < 10; i++){
+    const boxItem = document.createElement('div');
+    gridContainer.appendChild(boxItem).className = "grid-box";
+}
