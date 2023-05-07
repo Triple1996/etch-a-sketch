4c126e20f4e1a3e9565fa8fcdc7f5f0bfffa03ed
triple <45209813+Triple1996@users.noreply.github.com>
2023-05-07 01:00:20 -0400

    Grid working and begin adding functionality to redraw the grid with custom side lengths

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 const gridContainer = document.querySelector('#grid-container');
+const redrawGridBtn = document.querySelector('#redraw-grid-button');
 
-// create a 16 x 16 grid of divs
-for (let i = 0; i < 10; i++){
-    const boxItem = document.createElement('div');
-    gridContainer.appendChild(boxItem).className = "grid-box";
+let squaresPerSide = 16;
+
+redrawGridBtn.addEventListener('click', redrawGrid);
+
+function createGrid(rows, cols){
+    for (let i = 0; i < (rows * cols); i++){
+        const boxItem = document.createElement('div');
+        boxItem.addEventListener('mouseover', hoverEffect);
+        gridContainer.appendChild(boxItem).className = "grid-box";
+    }
 }
+
+function redrawGrid(){
+    let userNumSquares = prompt("enter number of squares per side");
+    squaresPerSide = userNumSquares;
+    gridContainer.style.setProperty('--number-rows', squaresPerSide);
+    gridContainer.style.setProperty('--number-cols', squaresPerSide);
+    console.log("changed sides");
+    deleteGrid();
+    createGrid(squaresPerSide, squaresPerSide);
+}
+
+function deleteGrid(){
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
+
+function hoverEffect(e){
+    console.log("hovering");   
+    e.target.style.background = 'red';
+}
+
+createGrid(squaresPerSide, squaresPerSide);
