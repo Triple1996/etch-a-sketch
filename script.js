38b73ba2eedf0c45a21842cffed8f05e01b900e5
triple <45209813+Triple1996@users.noreply.github.com>
2023-05-07 19:33:46 -0400

    Enable buttons to switch drawing modes

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
+const SHADING_DARKENING_RATE = 35;
+const STARTING_SQUARE_COLOR = "rgb(255, 255, 255)";
+
 const gridContainer = document.querySelector('#grid-container');
 const redrawGridBtn = document.querySelector('#redraw-grid-button');
 const resetBtn = document.querySelector('#reset-button');
-
-let mouseDown = false;
-let squaresPerSide = 16;
+const rainbowBtn = document.querySelector('#rainbow-button');
+const shadingBtn = document.querySelector('#shading-button');
 
 redrawGridBtn.addEventListener('click', redrawGrid);
 resetBtn.addEventListener('click', resetGrid);
+rainbowBtn.addEventListener('click', toggleRainbowMode);
+shadingBtn.addEventListener('click', toggleShadingMode);
+
+let shadingMode = false;
+let rainbowMode = true;
+let mouseDown = false;
+let squaresPerSide = 16;
 
 createGrid(squaresPerSide, squaresPerSide);
 
-
+function troll(){
+    console.error("test");
+}
 function createGrid(rows, cols){
     for (let i = 0; i < (rows * cols); i++){
         const boxItem = document.createElement('div');
         boxItem.addEventListener("mousedown", toggleMouseDown);
         boxItem.addEventListener("mouseup", toggleMouseUp);
         boxItem.addEventListener("mouseenter", hoverEffect);
-        boxItem.style.background = "rgb(255, 255, 255)";
+        boxItem.style.background = STARTING_SQUARE_COLOR;
         gridContainer.appendChild(boxItem).className = "grid-box";
     }
 }
@@ -37,29 +48,51 @@ function redrawGrid(){
 
 function hoverEffect(e){
 
-    //Rainbow mode
-    // if (mouseDown){
-    //     console.log(e.target.style.background);
-    //     let rgb1 = Math.floor(Math.random() * 255);
-    //     let rgb2 = Math.floor(Math.random() * 255);
-    //     let rgb3 = Math.floor(Math.random() * 255);
-
-    //     let color = "rgb(" + rgb1+ ", " + rgb2 + ", " + rgb3 + ")";
-    //     e.target.style.background = color;
-    // }
-
-    //Shading mode
     if (mouseDown){
-        currentColor = e.target.style.background;
-        let rgbVals = extractRgbVals(currentColor);
-        let currentRGB1 = rgbVals[0];
-        let currentRGB2 = rgbVals[1];
-        let currentRGB3 = rgbVals[2];
-
-        let newColor = "rgb(" + (currentRGB1 - 35) + ", " + (currentRGB2 - 35) + ", " + (currentRGB3 - 35) + ")";
-        e.target.style.background = newColor;
+        if (rainbowMode){
+            if (mouseDown){
+                let rgb1 = Math.floor(Math.random() * 255);
+                let rgb2 = Math.floor(Math.random() * 255);
+                let rgb3 = Math.floor(Math.random() * 255);
+    
+                let color = "rgb(" + rgb1+ ", " + rgb2 + ", " + rgb3 + ")";
+                e.target.style.background = color;
+            }
+        }
+        else if (shadingMode){
+            if (mouseDown){
+                currentColor = e.target.style.background;
+                let rgbVals = extractRgbVals(currentColor);
+                let currentRGB1 = rgbVals[0];
+                let currentRGB2 = rgbVals[1];
+                let currentRGB3 = rgbVals[2];
+    
+                let newColor = "rgb(" 
+                    + (currentRGB1 - SHADING_DARKENING_RATE) + ", " 
+                    + (currentRGB2 - SHADING_DARKENING_RATE) + ", " 
+                    + (currentRGB3 - SHADING_DARKENING_RATE) + ")";
+                e.target.style.background = newColor;
+            }
+        }
+        else {
+            console.error("rainbowMode and shadingMode both equal to false")
+        }
     }
+}
 
+function toggleRainbowMode(){
+    resetModes();
+    rainbowMode = true;
+}
+
+function toggleShadingMode(){
+    resetModes();
+    shadingMode = true;
+}
+
+function resetModes(){
+    rainbowMode = false;
+    shadingMode = false;
 }
 
 function extractRgbVals(color){
